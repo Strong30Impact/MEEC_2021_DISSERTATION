5a8dbcbdfc5ffb6b060cd75414d1bb13f88c5ff3
joaopfaria30 <joaopfaria30@outlook.com>
2022-07-20 16:21:15 +0100

    Automatic login in application done

--- Code/Mobile Application/app/src/main/java/pt/ipca/dissertation_14861/ui/activities/SplashScreenActivity.kt
+++ Code/Mobile Application/app/src/main/java/pt/ipca/dissertation_14861/ui/activities/SplashScreenActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import pt.ipca.dissertation_14861.R
 
 /*
@@ -11,18 +12,28 @@ import pt.ipca.dissertation_14861.R
 */
 class SplashScreenActivity : AppCompatActivity() {
 
+    var auth : FirebaseAuth? = null
+
     private val splashtime: Long = 3000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
+        auth = FirebaseAuth.getInstance()
+
         // Hide toolbar
         supportActionBar?.hide()
 
         Handler().postDelayed({
-            // Check if the user has read the intro of the App
-            startActivity(Intent(this, LoginActivity::class.java))
+            // Check if the user has authenticated or not
+            if(auth?.currentUser != null) {
+                startActivity(Intent(this, MainActivity::class.java))
+            } else {
+                startActivity(Intent(this, LoginActivity::class.java))
+            }
+/*            // Check if the user has read the intro of the App
+            startActivity(Intent(this, LoginActivity::class.java))*/
         }, splashtime)
     }
 }
