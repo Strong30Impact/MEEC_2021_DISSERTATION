276aee49b97fca56d7c2cec61ba455b8a82c3fe2
joaopfaria30 <joaopfaria30@outlook.com>
2022-07-22 11:54:02 +0100

    Changed navigation drawer to bottom navigation drawer and added popup whit more information

--- Code/Mobile Application/app/src/main/java/pt/ipca/dissertation_14861/ui/activities/MainActivity.kt
+++ Code/Mobile Application/app/src/main/java/pt/ipca/dissertation_14861/ui/activities/MainActivity.kt
@@ -1,47 +1,35 @@
 package pt.ipca.dissertation_14861.ui.activities
 
-import android.Manifest
 import android.app.AlertDialog
 import android.content.Intent
 import android.content.IntentFilter
-import android.content.pm.PackageManager
 import android.net.ConnectivityManager
-import android.net.Uri
-import android.os.Build
 import android.os.Bundle
-import android.os.Environment
-import android.provider.MediaStore
-import android.view.View
+import android.util.Log
 import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toolbar
-import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.app.AppCompatDelegate
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.fragment.app.FragmentTransaction
-import com.bumptech.glide.Glide
-import com.bumptech.glide.request.RequestOptions
-import com.google.android.material.navigation.NavigationView
+import androidx.appcompat.widget.PopupMenu
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
 import pt.ipca.dissertation_14861.R
 import pt.ipca.dissertation_14861.ui.fragments.*
 import pt.ipca.dissertation_14861.utils.Alerts
-import pt.ipca.dissertation_14861.utils.Firebase
 import pt.ipca.dissertation_14861.utils.connections.ConnectionReceiver
 import pt.ipca.dissertation_14861.utils.connections.ReceiverConnection
-import java.io.File
-import java.security.cert.Certificate
+
 
 class MainActivity : AppCompatActivity(), ConnectionReceiver.ConnectionReceiverListener {
 
     // Inicialization of the Project variables
     //toolbar
-    private lateinit var toolbar: androidx.appcompat.widget.Toolbar
+/*    private lateinit var toolbar: androidx.appcompat.widget.Toolbar
     private lateinit var drawerLayout: DrawerLayout
     private lateinit var navigationView: NavigationView
-    private lateinit var hview: View
+    private lateinit var hview: View*/
+
+    private lateinit var bottomNavigationView: BottomNavigationView
 
     //Username
     private lateinit var user_iv_photo: ImageView
@@ -52,11 +40,11 @@ class MainActivity : AppCompatActivity(), ConnectionReceiver.ConnectionReceiverL
     private lateinit var  auth : FirebaseAuth
 
     //Fragments
-    private lateinit var settingsFragment: SettingsFragment
-    private lateinit var mainFragment: MainFragment
-    private lateinit var gamepadJoystickFragment: GamepadJoystickFragment
-    private lateinit var trackingCameraFragment: TrackingCameraFragment
-    private lateinit var addProblemFragment: AddProblemFragment
+    private var settingsFragment: SettingsFragment = SettingsFragment()
+    private var mainFragment: MainFragment = MainFragment()
+    private var gamepadJoystickFragment: GamepadJoystickFragment = GamepadJoystickFragment()
+    private var trackingCameraFragment: TrackingCameraFragment = TrackingCameraFragment()
+    private var addProblemFragment: AddProblemFragment = AddProblemFragment()
 
     companion object{
         lateinit var name: String
@@ -76,8 +64,100 @@ class MainActivity : AppCompatActivity(), ConnectionReceiver.ConnectionReceiverL
         )
         ReceiverConnection.instance.setConnectionListener(this)
 
-        setUpNavigationDrawer()
+        bottomNavigationView = findViewById(R.id.nav_view)
+
+        supportFragmentManager.beginTransaction().replace(R.id.frame_Layout,mainFragment).commit()
+
+        bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                /*
+                    Button to open main
+                */
+                R.id.transport_icon -> {
+                    replaceFragment(mainFragment)
+                }
+                /*
+                Button to open transport list
+                */
+                R.id.transport_list_icon -> {
+                    replaceFragment(addProblemFragment)
+                }
+                /*
+                Button to open robot tracking camera
+                */
+                R.id.camera_icon -> {
+                    replaceFragment(trackingCameraFragment)
+                }
+                /*
+                Button to open gamepad joystick
+                */
+                R.id.ros_icon -> {
+                    replaceFragment(gamepadJoystickFragment)
+                }
+                /*
+                    Button to open more information
+                */
+                R.id.more_icon -> {
+                    openPopUp()
+                }
+            }
+            return@setOnItemSelectedListener true
+        }
+    //        setUpNavigationDrawer()
     }
+
+    /*
+        Function to open popup menu and show more information
+    */
+    private fun openPopUp() {
+
+        val popupMenu = PopupMenu(this, findViewById(R.id.more_icon))
+        popupMenu.setOnMenuItemClickListener { item ->
+            when (item.itemId) {
+                R.id.report_icon -> {
+                    replaceFragment(addProblemFragment)
+                    true
+                }
+                R.id.settings_icon -> {
+                    replaceFragment(settingsFragment)
+                    true
+                }
+                R.id.logout_icon -> {
+                    // clear user information
+                    name = ""
+                    nCertificate = ""
+                    FirebaseAuth.getInstance().signOut()
+                    val intent = Intent(this, LoginActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                else -> false
+            }
+        }
+
+        popupMenu.inflate(R.menu.popup_menu)
+
+        try {
+            val fieldMPopup = PopupMenu::class.java.getDeclaredField("mPopup")
+            fieldMPopup.isAccessible = true
+            val mPopup = fieldMPopup.get(popupMenu)
+            mPopup.javaClass
+                .getDeclaredMethod("setForceShowIcon", Boolean::class.java)
+                .invoke(mPopup, true)
+        } catch (e: Exception){
+            Log.e("Main", "Error showing menu icons.", e)
+        } finally {
+            popupMenu.show()
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.frame_Layout, fragment, null)
+            .addToBackStack(null)
+            .commit()    }
 
     /*
         Function that checks the Internet connection
@@ -90,52 +170,49 @@ class MainActivity : AppCompatActivity(), ConnectionReceiver.ConnectionReceiverL
         }
     }
 
-    private fun setUpNavigationDrawer() {
-        toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.main_toolbar)
+    /*   private fun setUpNavigationDrawer() {
+   //        toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.main_toolbar)
 
-        //hide toolbar
-        supportActionBar?.hide()
+           drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
+           navigationView = findViewById<NavigationView>(R.id.nav_view)
+           hview = navigationView.getHeaderView(0)
 
-        drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
-        navigationView = findViewById<NavigationView>(R.id.nav_view)
-        hview = navigationView.getHeaderView(0)
+           user_tv_id = hview.findViewById<TextView>(R.id.user_tv_id)
+           user_tv_name = hview.findViewById<TextView>(R.id.user_tv_name)
+           user_iv_photo = hview.findViewById<ImageView>(R.id.user_iv_photo)
 
-        user_tv_id = hview.findViewById<TextView>(R.id.user_tv_id)
-        user_tv_name = hview.findViewById<TextView>(R.id.user_tv_name)
-        user_iv_photo = hview.findViewById<ImageView>(R.id.user_iv_photo)
+           //Firebase info
+           auth = FirebaseAuth.getInstance()
 
-        //Firebase info
-        auth = FirebaseAuth.getInstance()
+           // Show user information
+           user_tv_name.text = name
 
-        // Show user information
-        user_tv_name.text = name
+           if(nCertificate == "Administrator"){
+               user_tv_id.text = nCertificate
+           } else {
+               user_tv_id.text = "Certificate Professional nº " + nCertificate
+           }
 
-        if(nCertificate == "Administrator"){
-            user_tv_id.text = nCertificate
-        } else {
-            user_tv_id.text = "Certificate Professional nº " + nCertificate
-        }
-
-/*        // rounded corner image
+   *//*        // rounded corner image
         Glide.with(this).load(image).override(400, 400).apply(RequestOptions.circleCropTransform()).into(
             user_iv_photo
-        )*/
+        )*//*
 
         user_tv_id = hview.findViewById<TextView>(R.id.user_tv_id)
         user_tv_name = hview.findViewById<TextView>(R.id.user_tv_name)
         user_iv_photo = hview.findViewById<ImageView>(R.id.user_iv_photo)
 
-        var actionBarDrawerToggle = ActionBarDrawerToggle(
+*//*        var actionBarDrawerToggle = ActionBarDrawerToggle(
             this,
             drawerLayout,
             toolbar,
             R.string.openNavDrawer,
             R.string.closeNavDrawer
-        )
+        )*//*
 
-        drawerLayout.addDrawerListener(actionBarDrawerToggle)
+*//*        drawerLayout.addDrawerListener(actionBarDrawerToggle)
         actionBarDrawerToggle.syncState()
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)*//*
 
         mainFragment = MainFragment()
         supportFragmentManager
@@ -144,14 +221,14 @@ class MainActivity : AppCompatActivity(), ConnectionReceiver.ConnectionReceiverL
             .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
             .commit()
 
-        /*
+        *//*
             Button Fuctions - Here the function is defined for each button
-         */
+         *//*
         navigationView.setNavigationItemSelectedListener{
             when (it.itemId) {
-                /*
+                *//*
                     Button to open add problems
-                */
+                *//*
                 R.id.add_icon -> {
                     addProblemFragment = AddProblemFragment()
                     supportFragmentManager
@@ -159,9 +236,9 @@ class MainActivity : AppCompatActivity(), ConnectionReceiver.ConnectionReceiverL
                         .replace(R.id.drawable_frameLayout, addProblemFragment, null).addToBackStack(null)
                         .commit()
                 }
-                /*
+                *//*
                     Button to open robot tracking camera
-                */
+                *//*
                 R.id.camera_icon -> {
                     trackingCameraFragment = TrackingCameraFragment()
                     supportFragmentManager
@@ -169,9 +246,9 @@ class MainActivity : AppCompatActivity(), ConnectionReceiver.ConnectionReceiverL
                         .replace(R.id.drawable_frameLayout, trackingCameraFragment, null).addToBackStack(null)
                         .commit()
                 }
-                /*
+                *//*
                     Button to open gamepad joystick
-                */
+                *//*
                 R.id.ros_icon -> {
                     gamepadJoystickFragment = GamepadJoystickFragment()
                     supportFragmentManager
@@ -179,9 +256,9 @@ class MainActivity : AppCompatActivity(), ConnectionReceiver.ConnectionReceiverL
                         .replace(R.id.drawable_frameLayout, gamepadJoystickFragment, null).addToBackStack(null)
                         .commit()
                 }
-                /*
+*//*                *//**//*
                     Button to open settings
-                 */
+                 *//**//*
                 R.id.settings_icon -> {
                     settingsFragment = SettingsFragment()
                     supportFragmentManager
@@ -189,9 +266,9 @@ class MainActivity : AppCompatActivity(), ConnectionReceiver.ConnectionReceiverL
                         .replace(R.id.drawable_frameLayout, settingsFragment, null).addToBackStack(null)
                         .commit()
                 }
-                /*
+                *//**//*
                     Button to log out
-                */
+                *//**//*
                 R.id.logout_icon -> {
                     // clear user information
                     name = ""
@@ -199,10 +276,10 @@ class MainActivity : AppCompatActivity(), ConnectionReceiver.ConnectionReceiverL
                     FirebaseAuth.getInstance().signOut()
                     val intent = Intent(this, LoginActivity::class.java)
                     startActivity(intent)
-                }
+                }*//*
             }
             drawerLayout.closeDrawers()
             true
         }
-    }
+    }*/
 }
